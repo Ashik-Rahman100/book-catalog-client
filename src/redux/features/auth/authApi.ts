a201ce8ae5d04503b024f9e00432014bcb18e694
Ashik-Rahman100 <ashik5515@gmail.com>
2023-08-20 20:08:12 +0600

    add wishList with redux api

--- src/redux/features/auth/authApi.ts
+++ src/redux/features/auth/authApi.ts
@@ -1,27 +1,38 @@
-import { IUserApiData } from "../../../types/globalTypes";
+import { IBook, IUserApiData } from "../../../types/globalTypes";
 import api from "../../api/apiSlice";
 
 const authApi = api.injectEndpoints({
   endpoints: (builder) => ({
     createUser: builder.mutation<IUserApiData, IUserApiData>({
+      // The query function receives user data and returns the fetch configuration
       query: (user) => ({
-        url: `/auth/signup`,
+        url: "/auth/signup",
         method: "POST",
         body: user,
       }),
     }),
-
     loginUser: builder.mutation<
       IUserApiData,
       { email: string; password: string }
     >({
       query: ({ email, password }) => ({
-        url: `/auth/login`,
+        url: "/auth/login",
         method: "POST",
         body: { email, password },
+      }),
+    }),
+    addToWishList: builder.mutation({
+      query: ({ id, book }: { id: string; book: IBook }) => ({
+        url: `/auth/add-to-wish/${id}`,
+        method: "PATCH",
+        body: { id, book },
       }),
     }),
   }),
 });
 
-export const { useCreateUserMutation, useLoginUserMutation } = authApi;
+export const {
+  useCreateUserMutation,
+  useLoginUserMutation,
+  useAddToWishListMutation,
+} = authApi;
